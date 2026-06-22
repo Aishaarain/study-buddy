b7adbead42a48d002b0650468d6a18048a3beeb2
Aishaarain <155973754+Aishaarain@users.noreply.github.com>
2026-06-23 00:55:38 +0500

    Update supabaseClient.js

--- frontend/src/lib/supabaseClient.js
+++ frontend/src/lib/supabaseClient.js
@@ -1,13 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-const supabaseanonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 console.log("Supabase URL exists:", Boolean(supabaseUrl));
 console.log("Supabase key exists:", Boolean(supabaseAnonKey));
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase environment variables')
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
 }
 
-export const supabase = createClient(supabaseanonKey, supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
